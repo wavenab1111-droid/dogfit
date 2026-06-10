ae03cab4d5e3b94aea7e98fc96a25edc6afedfb9
Hamin Choi <wavenab1111@gmail.com>
2026-06-10 16:12:43 +0900

    Update vite.config.ts for GitHub Pages build

--- vite.config.ts
+++ vite.config.ts
@@ -8,18 +8,13 @@ export default defineConfig({
     ? `/${process.env.GITHUB_REPOSITORY.split("/")[1]}/`
     : "/",
   plugins: [
-    // The React and Tailwind plugins are both required for Make, even if
-    // Tailwind is not being actively used – do not remove them
     react(),
     tailwindcss(),
   ],
   resolve: {
     alias: {
-      // Alias @ to the src directory
       '@': path.resolve(__dirname, './src'),
     },
   },
-
-  // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
   assetsInclude: ['**/*.svg', '**/*.csv'],
 })
